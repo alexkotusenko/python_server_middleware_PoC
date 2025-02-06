1df9232b5aad8bebfb16a827ed9c8041c7bad6ac
Alex Kotusenko <alexkotusenko@gmail.com>
2025-02-05 23:49:30 -0500

    Update run_python_middleware.sh

--- run_python_middleware.sh
+++ run_python_middleware.sh
@@ -7,3 +7,8 @@ touch $LOG
 echo "--------$(date)--------" >> $LOG
 
 echo "[INFO] $LOG file touched    $(date)" >> $LOG
+
+echo "[INFO] pip list: $(pip list)"
+
+echo "[INFO] running ./main.py..."
+python main.py
